b3f1294f73f777276f3d6b234ee2b46c2a8dedf2
Aurélien Dufour <Aurlien.Dufour@ensimag.grenoble-inp.fr>
2015-03-24 15:48:02 +0100

    Add factor

--- parse.R
+++ parse.R
@@ -1,12 +1,13 @@
 mydata = read.csv("/home/dufourau/3A/micro/data_final.csv",stringsAsFactors=FALSE) 
 #Add a column to store the previous beta 
-mydata$betaPrec <- 0
-
+mydata$betaPrec<- 0
+mydata$AIMPrec <- 0
 currentStockNumber<-mydata$stock_number[1];
 currentYear<- mydata$year[1];
 currentMonth<- mydata$month[1];
 mydata$betaPrec[1] <- NA;
-
+mydta$AIMPrec[1] <- NA;
+precAIM<-mydata$AIM[1];
 precBeta<-mydata$beta[1];
 #Loop over all rows
 for(i in 2:nrow(mydata)-1){
@@ -17,10 +18,12 @@ for(i in 2:nrow(mydata)-1){
     #Check if the date are fit
     if((currentYear == mydata$year[i]-1 && currentMonth == 12) || (currentYear == mydata$year[i] && currentMonth == mydata$month[i]-1) ){
       
-      mydata$betaPrec[i]= precBeta; 
+      mydata$betaPrec[i]= precBeta;
+      mydta$AIMPrec[i]= precAIM;
     
     }else{
       mydata$betaPrec[i]= NA;
+      mydta$AIMPrec[i]= NA;
       
     }
   #New asset
@@ -29,6 +32,7 @@ for(i in 2:nrow(mydata)-1){
   }  
   
   precBeta<-mydata$beta[i];
+  precAIM<-mydata$AIM[i];
   currentYear<- mydata$year[i];
   currentMonth<- mydata$month[i];
 }
--- reg1.R
+++ reg1.R
@@ -3,14 +3,17 @@ mydata = read.csv("/home/dufourau/3A/micro/data.csv",stringsAsFactors=FALSE)
 
 nextStockNumber<-mydata$stock_number[1];
 
-rentaVector<-NULL;
-aimVector<-NULL;
-betaVector<-NULL;
 
-#Loop over all yearsmin(mydata$year)
-for(i in 1990:1990){
+regVector<-NULL;
+
+
+#Loop over all 
+for(i in min(mydata$year):max(mydata$year)){
   #Loop over all month
-  for(j in 3:3){
+  for(j in 1:12){
+    rentaVector<-NULL;
+    aimVector<-NULL;
+    betaVector<-NULL;
     #Loop over all rows
     for(z in 2:nrow(mydata)-1){
       
@@ -18,15 +21,23 @@ for(i in 1990:1990){
         if(mydata$year[z]== i && mydata$month[z] == j && !is.na(mydata$betaPrec[z])){
           betaVector<-c(betaVector,mydata$betaPrec[z]);
           #aimVector<-c(aimVector,mydata$AIM[z]);
-          rentaVector<-c(rentaVector,mydata$return_rf[z]);
+          rentaVector<-c(rentaVector,as.numeric(mydata$return_rf[z]));
         }
       
       
       
     }
-    reg<- lm(rentaVector~betaVector);
-    summary(reg);
+    if(!is.null(rentaVector) &&  !is.null(betaVector)){
+      #Regression between r and beta
+      reg<- lm(rentaVector~betaVector);
+      regVector<-c(regVector,summary(reg)$coefficients[2])
+    }
     
   }
 }
+
+t.test(regVector, mu=0);
+
+
+
 
