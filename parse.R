cbdc1e070a21d8a8c58735b6e8db6d9e9d9995e1
Aurélien Dufour <Aurlien.Dufour@ensimag.grenoble-inp.fr>
2015-03-25 14:47:25 +0100

    Add aim

--- parse.R
+++ parse.R
@@ -1,44 +1,71 @@
-mydata = read.csv("/home/dufourau/3A/micro/data_final.csv",stringsAsFactors=FALSE) 
+dataFilename=paste(getwd(),"/data_final.csv", sep="")
+dataFilenameFacteurs=paste(getwd(),"/facteurs.csv", sep="")
+mydata = read.csv(dataFilename,stringsAsFactors=FALSE) 
+dataFacteurs = read.csv(dataFilenameFacteurs,stringsAsFactors=FALSE) 
 #Add a column to store the previous beta 
-mydata$betaPrec<- 0
-mydata$AIMPrec <- 0
+mydata$betaPrec <- NA
+mydata$AIMPrec <- NA
+mydata$PINPrec <- NA
+mydata$BTMPrec <- NA
+mydata$LogCapMarketPrec <- NA
+#mydata$fsrb <- 0
+#mydata$turnover <- 0
+
 currentStockNumber<-mydata$stock_number[1];
 currentYear<- mydata$year[1];
 currentMonth<- mydata$month[1];
-mydata$betaPrec[1] <- NA;
-mydta$AIMPrec[1] <- NA;
-precAIM<-mydata$AIM[1];
+
+#mydata$betaPrec[1] <- NA;
+#mydata$AIMPrec[1] <- NA;
+#mydata$PINPrec[1] <- NA;
+#mydata$BTMPrec[1] <- NA;
+#mydata$LogCapMarketPrec[1] <- NA;
+
+mydata[["rf"]] = 0;
+
 precBeta<-mydata$beta[1];
+precAIM<-mydata$AIM[1];
+precPIN<-mydata$pin[1];
+precBTM<-mydata$log_btm[1];
+precLogCapMarket<-mydata$log_market_cap[1];
 #Loop over all rows
 for(i in 2:nrow(mydata)-1){
+  
   mydata$stock_number[i];
+  # Get corresponding rf
+  indFacteur = intersect(which(dataFacteurs$year ==currentYear),which(dataFacteurs$month ==currentMonth))
+  rf= as.numeric(dataFacteurs$Risk.Free.Return[indFacteur]);
+  mydata$rf[i] = rf;
+  
   #Same asset
   if(mydata$stock_number[i]==currentStockNumber){
     
     #Check if the date are fit
-    if((currentYear == mydata$year[i]-1 && currentMonth == 12) || (currentYear == mydata$year[i] && currentMonth == mydata$month[i]-1) ){
+    if((currentYear == mydata$year[i]-1 && currentMonth == 12) 
+       || (currentYear == mydata$year[i] && currentMonth == mydata$month[i]-1) ){
       
-      mydata$betaPrec[i]= precBeta;
-      mydta$AIMPrec[i]= precAIM;
-    
-    }else{
-      mydata$betaPrec[i]= NA;
-      mydta$AIMPrec[i]= NA;
+      mydata$betaPrec[i]= precBeta; 
+      mydata$AIMPrec[i]= precAIM;
+      mydata$PINPrec[i]= precPIN;
+      mydata$BTMPrec[i]= precBTM;
+      if (precLogCapMarket != "."){
+        mydata$LogCapMarketPrec[i]= precLogCapMarket;
+      }
       
     }
-  #New asset
+    #New asset
   }else{
-      currentStockNumber= mydata$stock_number[i];
+    currentStockNumber= mydata$stock_number[i];
   }  
   
   precBeta<-mydata$beta[i];
   precAIM<-mydata$AIM[i];
+  precPIN<-mydata$pin[i];
+  precBTM<-mydata$log_btm[i];
+  precLogCapMarket<-mydata$log_market_cap[i];
   currentYear<- mydata$year[i];
   currentMonth<- mydata$month[i];
 }
 
 print("Write in data.csv");
-write.csv(mydata,file="/home/dufourau/3A/micro/data.csv");
-
-
-  
+write.csv(mydata,file=paste(getwd(),"/data.csv", sep=""));
--- reg1.R
+++ reg1.R
@@ -5,6 +5,8 @@ nextStockNumber<-mydata$stock_number[1];
 
 
 regVector<-NULL;
+regVector1<-NULL;
+regVectorPond<-NULL;
 
 
 #Loop over all 
@@ -18,19 +20,24 @@ for(i in min(mydata$year):max(mydata$year)){
     for(z in 2:nrow(mydata)-1){
       
       
-        if(mydata$year[z]== i && mydata$month[z] == j && !is.na(mydata$betaPrec[z])){
+        if(mydata$year[z]== i && mydata$month[z] == j && !is.na(mydata$betaPrec[z]) && mydata$betaPrec[z] !="." ){
           betaVector<-c(betaVector,mydata$betaPrec[z]);
-          #aimVector<-c(aimVector,mydata$AIM[z]);
+          aimVector<-c(aimVector,as.numeric(mydata$AIMPrec[z]));
           rentaVector<-c(rentaVector,as.numeric(mydata$return_rf[z]));
         }
       
       
       
     }
-    if(!is.null(rentaVector) &&  !is.null(betaVector)){
+    
+    if(!is.null(rentaVector) &&  !is.null(betaVector) && !is.null(aimVector) ){
       #Regression between r and beta
-      reg<- lm(rentaVector~betaVector);
+      reg<- lm(rentaVector~betaVector+aimVector);
+      #RegVector
       regVector<-c(regVector,summary(reg)$coefficients[2])
+      regVector1<-c(regVector1,summary(reg)$coefficients[3])
+      #RegVector pondéré par 1/Sd
+      regVectorPond<-c(regVectorPond,summary(reg)$coefficients[2]/summary(reg)$coef[[4]])
     }
     
   }
